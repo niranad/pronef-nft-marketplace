8ef35c7a0077b2753ecac2edee301159140265d2
Adeniran J. Olukanni <adesite67@gmail.com>
2023-01-19 17:38:23 +0100

    Fully implement Home and Detail screens and publish

---
+++ components/index.js
@@ -0,0 +1,8 @@
+import { CircleButton, RectButton } from './Buttons';
+import { SubInfo } from './SubInfo';
+export { default as FocusedStatusBar } from './FocusedStatusBar';
+export { default as HomeHeader } from './HomeHeader';
+export { default as NFTCard } from './NFTCard';
+export { default as DetailsDesc } from './DetailsDesc';
+export { default as DetailsBid } from './DetailsBid';
+export { CircleButton, RectButton, SubInfo }
